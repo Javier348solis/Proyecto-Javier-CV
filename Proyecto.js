44be74e7251f023c00764c3ca4f6b75b9055c123
Javier348solis <javiersolisretana@gmail.com>
2024-05-01 11:46:27 -0600

    New changes

---
+++ Proyecto.js
@@ -0,0 +1,35 @@
+let btn1 = document.getElementById("btn1").onclick = function () {
+    boton1()
+}
+function boton1() {
+    
+        alert("60% equivale a b2")
+}
+let btn2 = document.getElementById("btn2").onclick = function () {
+    boton2()
+}
+function boton2() {
+    
+        alert("90% equivale a level superior senior")
+}
+let btn3 = document.getElementById("btn3").onclick = function () {
+    boton3()
+}
+function boton3() {
+    
+        alert("75% equivale a b2+")
+}
+let btn4 = document.getElementById("btn4").onclick = function () {
+    boton4()
+}
+function boton4() {
+    
+        alert("70% equivale a level senior")
+}
+let btn5 = document.getElementById("btn5").onclick = function () {
+    boton5()
+}
+function boton5() {
+    
+        alert("55% equivale a level superior junior")
+}
